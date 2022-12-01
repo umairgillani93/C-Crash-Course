e46fe47d65dcbfa90045fc0ec2a5148b839820de
umairgillani93 <umairgillani93@gmail.com>
2022-12-01 23:50:52 +0500

    new code added

---
+++ statements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	cin >> x;
+	cin >> y;
+
+	if (x > y) {
+		cout << "True" << '\n';
+	}
+
+	else if (x > y) {
+		cout << "false" << '\n';
+	}
+	else {cout << "both are equal" << '\n';}
+}
+
