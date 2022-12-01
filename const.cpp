6cdde2c374646669d375266d16624e90dbae234d
umairgillani93 <umairgillani93@gmail.com>
2022-12-01 22:02:20 +0500

    new code added

---
+++ const.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	const int x = 29349;      // not going to be overridden
+	cout << "constant integer is : " << x << '\n';
+	return x;
+
+							  
+}
---
+++ in_out.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int x;
+	float y;
+
+	cin >> x;
+	cout << "integer x is: " << x << '\n';
+	cin >> y;
+	cout << "float y is: " << y << '\n';
+
+	return 0;
+
+}
