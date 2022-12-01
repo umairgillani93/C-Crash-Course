76506bbfe2fe5d69b3ac85ab472791ea730ed6d6
umairgillani93 <umairgillani93@gmail.com>
2022-12-01 22:10:00 +0500

    new code added

---
+++ arthmatics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int x;
+	x = 10;
+
+	--x;
+	cout << "x is: " << x << '\n';
+	++x;
+	cout << "x is: " << x << '\n';
+	return 0;
+}
