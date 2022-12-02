ed5dea114f1421ac59770155552100cd6216e259
umairgillani93 <umairgillani93@gmail.com>
2022-12-02 22:42:48 +0500

    new code added

--- vector.cpp
+++ vector.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-  vector<int> v1;
-  long end = 100;
-  for (int i = 0; i <= end; i++){
-    v1.push_back(i);
-  }
+int main() {
+	vector<int> vec1;
+	for (int i = 0; i < 10; i++) {
+		vec1.push_back(i);
+	}
 
-  for (int j = 0; j <= v1.size(); j++) {
-    cout << "item is: " << v1[j] << "\n";
-  }
-  cout << "vector begins" << v1.begin() << "\n";
-  cout << "vector ends" << v1.end() << "\n";
-  return 0;
-  
-    
+	for (int j = 0; j < 10; j++) {
+		cout << vec1[j] << '\n';
+	}
+
+	cout << vec1.begin() << '\n';
+	cout << vec1.end() << '\n';
+
+	
+
 }
-  
