d24f1d7551fb0641f92d8d2f4677bce15b5676eb
umairgillani93 <umairgillani93@gmail.com>
2022-12-05 23:29:53 +0500

    new code added

--- algos_implementation/linear_search.cpp
+++ algos_implementation/linear_search.cpp
@@ -1,40 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ls(vector<int> arr, int n) {
+	int i = 0;
+	while (i < arr.size()) {
+		if (arr[i] == n) {
+			cout << "found element at index:  " << i << '\n';
+			return i;
+		}
+		else {
+			i++;
+		}
+	}
+	return 0;
+}
+
 
 int main() {
 	// linear search implementation
 	// takes two items; an array and number
 	// returns the index of number; if present in given array
-	// lets take the array and number from user initially
-	//
-
-	int size, numb;
+	
+	int size;
 	int n;
-	cin >> n;
 	vector<int> v;
 	cin >> size;
 
 	for (int i = 0; i < size; i++) {
-		cin >> numb;
-		v.push_back(numb);
+		cin >> n;
+		v.push_back(n);
 	}
-
+	
 	for (int j = 0; j < size; j++) {
-		cout << v[j] << " ";
+		cout << v[j] << '\n';
 	}
 
-	int i = 0;
-	while (i < v.size()) {
-		if (v[i] == n) {
-			cout << "index of item is " << i << "\n";
-			return i;
-		}
-		else {
-			++i;
-		}
-	}
-
-	return 0;
+	cout << ls(v, n) << '\n';
 	
 }	
