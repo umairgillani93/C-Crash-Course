f46342474e9c05dc3c9ce1a2b2fdd29ce5cc479a
umairgillani93 <umairgillani93@gmail.com>
2022-12-09 23:27:46 +0500

    new code added

--- file.cpp
+++ file.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<int> test(vector<int> test_vector) {
+	vector<int> new_vector;
+	for (int i = 0; i < test_vector.size(); i++) {
+		new_vector.push_back(test_vector[i] * test_vector[i]);
+	}
+
+	return new_vector;
+}
+
+//vector<vector<int>> inside(int n) {
+//	vector<int> ret_vector;
+//	for (int i = 0; i < n; i++){
+//		vector<int> in_vector;
+//		cin >> in_vector;
+//		ret_vector.push_back(in_vector);
+//		
+//	}
+//
+//	return ret_vector;
+//}
+
+
+
 int main() {
-	int arr[] = {3,2,3,4,3};
-	cout << sizeof(arr) << '\n';
-	cout << sizeof(arr[0]) << '\n';
+	vector<int> v(5, 10);
+
+	for (int i = 0; i < v.size(); ++i) {
+		cout << v[i] << '\n';
+	}
+
+	vector<int>::iterator it = v.begin();
+	cout << *v.begin() << '\n';
+	cout << *it << '\n';
+	return 0;
+
 }	
---
+++ ptr1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int i = 10;
+	int *p = &i;
+
+	cout << i << '\n';
+	cout << *p << '\n';
+	cout << &p << '\n';
+
+	return 0;
+}
