081d6f2d31e03ff341b780a2d0cfeac9950ea7e2
umairgillani93 <umairgillani93@gmail.com>
2022-12-27 23:36:46 +0500

    pointers & addresses

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
-#include<conio.h>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  cout << "Gonna hold for some time ... !! " << endl;
-  getch();
-  return 0;
+int main() {
+	vector<int> v{1,2,3,4,5};
+	for (auto it = v.begin(); it != v.end(); it++) {
+		cout << *it << '\n';
+	}
+
+	for (auto it = v.begin(); it != v.end(); it++) {
+		cout << "Address: " << &it << '\n';
+	}
+	
 }
