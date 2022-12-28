44ec29f76f251740b156f482735d9829a98392c5
umairgillani93 <umairgillani93@gmail.com>
2022-12-29 00:16:26 +0500

    code added

--- algos_implementation/bin_search.cpp
+++ algos_implementation/bin_search.cpp
@@ -2,31 +2,69 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int bsi(vector<int> arr, int low, int high, int x) {
-	int mid;
-	mid = low + (low + high) / 2;
-	while (low <= high) {
-		if (arr[mid] == x) {
-			return mid;
-		}
-		if (arr[mid] < x) {
-			low = mid + 1;
-		}
-		else {
-			high = mid -1;
-		}
-		
-	}
-	return -1;
-}
-
+//int bsi(vector<int> arr, int low, int high, int x) {
+//	int mid;
+//	mid = low + (low + high) / 2;
+//	while (low <= high) {
+//		if (arr[mid] == x) {
+//			return mid;
+//		}
+//		if (arr[mid] < x) {
+//			low = mid + 1;
+//		}
+//		else {
+//			high = mid -1;
+//		}
+//		
+//	}
+//	return -1;
+//}
+//
 
 int main() {
+	vector<int> v;
+	cout << "enter the size " << '\n';
+	int size;
+	cin >> size;
+	for (int i = 0; i < size; i++) {
+		cout << "Enter integers to the vector " << '\n';
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
 	int n;
-	n = 3;
-	vector<int> v {1,2,3,4,5};
+	cout << "enter number to find in array " << '\n';
+	cin >> n;
+	cout << "done " << '\n';
+	vector<int> first;
+	vector<int> second;
+	int mid;
+	mid = v[v.size() - 1] / 2;
+	cout << "mid: " << mid << '\n';
+	for (int i = 0; i < mid; i++) {
+		first.push_back(v[i]);
+	}
+	for (int j = mid; j < v.size(); j++) {
+		second.push_back(v[j]);
+	}
 
-	cout << bsi(v, 0, 9, 8) << '\n';
+	for (int i = 0; i < first.size(); i++) {
+		cout << first[i] << '\n';
+
+	}
+	for (int j = 0; j < second.size(); j++) {
+		cout << second[j] << '\n';
+
+	}
+	
+	if (v[mid] == n) {
+		return mid;
+	}
+	else if (v[mid] < n) {
+
+	}
+
 	return 0;
 
 }
