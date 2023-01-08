f719c6469fd16deb47e7e8e4b1f506120ce94065
umairgillani93 <umairgillani93@gmail.com>
2023-01-08 18:35:00 +0500

    code added

--- data_structures_algorithms_cpp/str.cpp
+++ data_structures_algorithms_cpp/str.cpp
@@ -11,6 +11,29 @@ struct Rectangle {
 	int breath; // takes 2-bytes
 };
 
+struct Complex {
+	// defining complex number using Structure
+	// a + ib -> a, b are the real numbers 
+	// ib -> collectively is an immaginary number
+	int real;
+	int img;
+
+};
+
+struct Student {
+	int age; // student's age
+	char cls[10]; // student's class
+	char first[10]; // first name
+	char last[10]; // last name
+	char dept[20]; // department
+	char address[50]; // address
+	
+	// memory consumed in bytes; age = 4, cls = 10, first = 10, second = 10, dept = 20, address = 50;
+	// 4 + 10 + 10 + 10 + 20 + 50 = 104 bytes
+
+}
+
+
 int main() {
 	struct Rectangle r{10, 15}; // declare + initialize the structure
 	cout << r.len << '\n';
