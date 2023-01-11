7e68989ae3a1ed2d26ad92c0281dc4d67fbc150c
umairgillani93 <umairgillani93@gmail.com>
2023-01-11 23:07:14 +0500

    passing structure agr by reference

--- data_structures_algorithms_cpp/str_arg.cpp
+++ data_structures_algorithms_cpp/str_arg.cpp
@@ -12,11 +12,19 @@ int area(struct Rectangle r) {
 	return r.len * r.breath;
 }
 
+int area_ref(struct Rectanble &r) {
+	// passing argument by reference doesn't create a new object
+	// instead it makes changes to actual paramters
+	
+	return r.len * r.breath;
+}
+
 
 int main() {
 
 	struct Rectangle r1 {10, 15};
-	cout << area(r1) << '\n';
+	cout << area(r1) << '\n'; // actual paramters are not changed
+	cout << area_ref(r1) << '\n'; // actual paramters are changed
 	return 0;
 
 }
