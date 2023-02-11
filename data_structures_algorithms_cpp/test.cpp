22bf57ecbbaf26e5fc85a18147e8c14e337f609c
umairgillani93 <umairgillani93@gmail.com>
2023-02-11 16:40:41 +0500

    linked list implementation C++

--- data_structures_algorithms_cpp/test.cpp
+++ data_structures_algorithms_cpp/test.cpp
@@ -2,41 +2,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 struct Node {
 	int data;
 	struct Node *next;
+
 };
 
+
 int main() {
-	int *p;
-	struct Node *head;
 	struct Node *first;
 	struct Node *second;
 	struct Node *third;
 
-	head -> data = 10;
-	first -> data = 20;
-	second -> data = 30;
-	third -> data = 40;
-
-
-	// lets link all the nodes now
-	head -> next = first;
+	first -> data = 10;
 	first -> next = second;
+	second -> data = 20;
 	second -> next = third;
+	third -> data = 30;
 	third -> next = NULL;
 
-	// point pointer 'p' to head node
-
-	cout << head -> data << '\n';
 	cout << first -> data << '\n';
-	cout << second -> data << '\n';
-	cout << third -> data << '\n';
-
-	cout << head -> next << '\n';
 	cout << first -> next << '\n';
+	cout << second -> data << '\n';
 	cout << second -> next << '\n';
-	cout << third -> next << '\n';
+	cout << third-> data << '\n';
+	cout << third -> next<< '\n';
 
 	return 0;
 }
