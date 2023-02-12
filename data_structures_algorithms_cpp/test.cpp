cba923cc33c2ea79ba762ce58fc0b67d546f24c7
umairgillani93 <umairgillani93@gmail.com>
2023-02-12 20:01:34 +0500

    Linked list

--- data_structures_algorithms_cpp/test.cpp
+++ data_structures_algorithms_cpp/test.cpp
@@ -2,32 +2,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// Create a Node structure
 struct Node {
 	int data;
 	struct Node *next;
-
 };
 
+// initilize the head Node pointer globally
+struct Node *head;
+
+
+// Create a linked-list
+void create(int arr[], int n) {
+	struct Node *last;
+
+	head -> data = arr[0];
+	head -> next = NULL;
+	last -> next = NULL;
+	last = head;
+
+	for (int i = 1; i < n; i++) {
+		struct Node *middle;
+		middle -> data = arr[i];
+		middle -> next = NULL;
+		last -> next = middle;
+		last = middle;
+	}
+
+}
+
+void display(struct Node *p) {
+	while (!p) {
+		cout << p -> data << '\n';
+		p = p -> next;
+	}
+
+}
+
+int count_nodes(struct Node *p) {
+	int c = 0;
+	while (!p) {
+		++c;
+		p = p -> next;
+	}
+
+	return c;
+}
+
+Node * search(struct Node *p, int key) {
+	while (!p) {
+		if (key == p -> data) {
+			return p -> next;
+		}
+	}
+
+	return NULL;
+}
 
 int main() {
-	struct Node *first;
-	struct Node *second;
-	struct Node *third;
-
-	first -> data = 10;
-	first -> next = second;
-	second -> data = 20;
-	second -> next = third;
-	third -> data = 30;
-	third -> next = NULL;
-
-	cout << first -> data << '\n';
-	cout << first -> next << '\n';
-	cout << second -> data << '\n';
-	cout << second -> next << '\n';
-	cout << third-> data << '\n';
-	cout << third -> next<< '\n';
-
+	int arr[] = {1, 3, 5, 7, 9};
+	int size = 5;
+	create(arr, size);
+	display(head);
 	return 0;
-}
+}	
