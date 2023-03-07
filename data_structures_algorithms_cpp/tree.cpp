0059ef90140fa4a84bd1bd87d6af934f8b1b84f2
umairgillani93 <umairgillani93@gmail.com>
2023-03-07 22:12:18 +0500

    Tree implementing using Queue in C++

--- data_structures_algorithms_cpp/tree.cpp
+++ data_structures_algorithms_cpp/tree.cpp
@@ -46,22 +46,96 @@ int dequeue(Queue *q) {
 	return x;
 }
 
+bool isEmpty(Queue *q){
+	if (q -> front == q -> rear) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
+
+bool isFull(Queue *q) {
+	if (q -> rear == q -> size - 1) {
+		return true;
+	}
+	 
+	else {
+		return false;
+	}
+}
+
 void display(Queue q) {
 	for (int i = q.front + 1; i <= q.rear; i++) {
 		cout << q.Q[i] << '\n';
 	}
 }
 
+// Tree Implemetation
+struct Node {
+	struct Node *l_child;
+	struct Node *r_child;
+	int data;
+};
+
+struct Node *root; // global Node pointer
+
+void create_tree() {
+	Node *p, *t;
+	int x; 
+	struct Queue q;
+
+	// create root Node
+	cout << "Enter root value : " << '\n';
+	cin >> x;
+
+	// create root now
+	root = (struct Node *)malloc(sizeof(struct Node));
+	root -> data = x;
+	root -> l_child = 0;
+	root -> r_child = 0;
+	
+	// insert root pointer in Queue
+	enqueue(root);
+
+	// check if queue is not empty
+	while (!isEmpty(q)) {
+		p = dequeue(&q); // assign item from Queue to poitner 'p'
+		cout << "Enter left child : " << '\n';
+		cin >> x;
+
+		if (x != -1) {
+			t = (struct Node *)malloc(sizeof(struct Node));
+			t -> data = x;
+			t -> l_child = t -> r_child = 0;
+			p -> l_child = t;
+			enqueue(t);
+
+		}
+
+		cout << "Enter right child : " << '\n';
+		cin >> x;
+
+		if (x != -1) {
+			t = (struct Node *)malloc(sizeof(struct Node));
+			t -> data = x;
+			t -> l_child = 0;
+			t -> r_child = 0;
+			p -> r_child = t;
+			enqueue(t);
+
+		}
+	}
+	
+}
+
 int main() {
 	struct Queue q;
-	// create a queue
-	create(&q, 5);
-	enqueue(&q, 10);
-	enqueue(&q, 20);
-	enqueue(&q, 30);
-	enqueue(&q, 40);
-	dequeue(&q);
-	dequeue(&q);
-	display(q);
+	struct Node tree;
+	create_tree();
 	return 0;
 }
+
+
+
