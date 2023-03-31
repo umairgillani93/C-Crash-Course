7615b53398f4180fbe1c89f0a9543ec8a731f436
umairgillani93 <umairgillani93@gmail.com>
2023-03-31 23:37:13 +0500

    palindrome reorder

---
+++ reorder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s = "AAAACACBA";
+
+	string new_s;
+
+	for (int i = 0; i < s.size(); i++) {
+		new_s += s[i];
+	}
+
+	return 0;
+}
