54da7a96ab29122ae0eed96070a3b151eb77b960
umairgillani93 <umairgillani93@gmail.com>
2023-05-23 23:04:52 +0500

    new problem

---
+++ algos_implementation/rep.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	string s = "some string";
+
+	int rep = 0;
+	int max = 0;
+	for (int i = 0; i < s.size(); i++) {
+		
+	}
+
+}
+
+
+// we have a string = "some string";
+// we need to find the maximum number of time a character has repeated
+// we'll have to initialize a counter
+// c = 0;
+// c++; when 
