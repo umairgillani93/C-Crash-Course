20c50277b1fab502acf81b351de91e90cedd6dd1
umairgillani93 <umairgillani93@gmail.com>
2023-05-27 17:21:25 +0500

    max item in an array

--- algos_implementation/r.cpp
+++ algos_implementation/r.cpp
@@ -1,17 +1,39 @@
+// most repeated character
+
 #include <iostream>
 #include <bits/stdc++.h>
-#include <fstream>
 
 
 using namespace std;
 
 int main() {
-	string t;
-	ifstream MyString("file.txt");
+	string s;
+	getline(cin, s);
+	
+	// sort the string first of all
+	sort(s.begin(), s.end());
 
-	while(getline(MyString, t)) {
-		cout << t  << '\n';
+	int curr = 1;
+	int best = 1; 
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == s[i + 1]) {
+			curr++;
+		}
+
+		else {
+			if (curr > best) {
+				best = curr;
+				curr = 1;
+			}
+		}
 	}
 
+	cout << best << '\n';
 	return 0;
 }
+
+// s = "aaabbc";
+// s[0] == s[1] -> curr = 2; 
+// curr = 2; best = 1; curr > best; best = curr; best = 2;
+//
--- algos_implementation/t.cpp
+++ algos_implementation/t.cpp
@@ -4,14 +4,33 @@
 using namespace std;
 
 int main() {
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
+	// s = "aaa";
+	// s[i] == s[i + 1];
+	// cur++;
+	// if s[i] != s[i + 1];
+	// if (curr > best);
+	// if yes -> best = curr;
+	// curr = 1;
+	
+	string s = "aaabbbb";
+	
+	int curr = 1;
+	int best = 1;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == s[i + 1]) {
+			curr++;
+			//cout << curr << '\n';
+		}
 
-	for (auto x: v) {
-		cout << x << '\n';
+		else {
+			if (curr > best) {
+				best = curr;
+				curr = 1;
+			}
+		}
 	}
+	
+	cout << best << '\n';
 
 	return 0;
 }
